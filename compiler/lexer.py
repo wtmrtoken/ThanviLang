aae5304444c888b7b997e51f3eb01b6c3781224c
Vhydjnb <113295706+Vhydjnb@users.noreply.github.com>
2026-07-12 16:38:06 +0530

    Add Lexer class for ThanviLang compiler

---
+++ compiler/lexer.py
@@ -0,0 +1,12 @@
+# ThanviLang (TVL) Compiler
+# Lexer
+# Founder: Meeravali Velupuri
+
+class Lexer:
+    def __init__(self, source):
+        self.source = source
+        self.position = 0
+
+    def tokenize(self):
+        print("TVL Lexer Started")
+        return []
