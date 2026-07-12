93edbc7951297b22e77f42b316d2031ac3f33a33
Vhydjnb <113295706+Vhydjnb@users.noreply.github.com>
2026-07-12 17:10:49 +0530

    Add AST node definitions for TVL

--- compiler/ast.py
+++ compiler/ast.py
@@ -1,23 +1,32 @@
-# ThanviLang (TVL) Compiler
-# AST (Abstract Syntax Tree)
+# ThanviLang (TVL)
+# AST
 # Founder: Meeravali Velupuri
 
 class ASTNode:
-    def __init__(self, node_type):
-        self.node_type = node_type
+    pass
+
 
 class Program(ASTNode):
     def __init__(self):
-        super().__init__("Program")
-        self.body = []
+        self.statements = []
 
-class Variable(ASTNode):
+
+class StoreStatement(ASTNode):
     def __init__(self, name, value):
-        super().__init__("Variable")
         self.name = name
         self.value = value
 
-class Print(ASTNode):
+
+class ShowStatement(ASTNode):
     def __init__(self, value):
-        super().__init__("Print")
         self.value = value
+
+
+class Number(ASTNode):
+    def __init__(self, value):
+        self.value = value
+
+
+class Identifier(ASTNode):
+    def __init__(self, name):
+        self.name = name
