b80a5568780bb361a5f06d0bef4c3a998ad7a63d
Vhydjnb <113295706+Vhydjnb@users.noreply.github.com>
2026-07-12 17:32:09 +0530

    Add TVL Optimizer
    Added an Optimizer class with a placeholder optimize method.

---
+++ compiler/optimizer.py
@@ -0,0 +1,18 @@
+# ThanviLang (TVL)
+# Optimizer
+# Founder: Meeravali Velupuri
+
+class Optimizer:
+
+    def optimize(self, program):
+
+        print("TVL Optimizer Started")
+
+        # Future optimizations:
+        # - Constant Folding
+        # - Dead Code Elimination
+        # - Constant Propagation
+        # - Peephole Optimization
+        # - Tail Call Optimization
+
+        return program
