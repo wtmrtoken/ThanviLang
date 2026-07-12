99124d722c6a5c34977c0bdb5aa20054c31db7d0
Vhydjnb <113295706+Vhydjnb@users.noreply.github.com>
2026-07-12 17:34:16 +0530

    Add TVL Version Information

---
+++ compiler/version.py
@@ -0,0 +1,12 @@
+# ThanviLang (TVL)
+# Version Information
+
+LANGUAGE = "ThanviLang"
+SHORT_NAME = "TVL"
+VERSION = "0.1.0"
+
+FOUNDER = "Meeravali Velupuri"
+
+COMPILER = "TVL Compiler"
+
+VM = "TVM"
