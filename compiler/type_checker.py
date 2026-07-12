33927550200ec9fd3b9fb14baa555334a9483305
Vhydjnb <113295706+Vhydjnb@users.noreply.github.com>
2026-07-12 17:20:09 +0530

    Implement TVL Type Checker
    Removed unnecessary comments and simplified the type checking logic.

--- compiler/type_checker.py
+++ compiler/type_checker.py
@@ -1,36 +1,12 @@
-# ThanviLang (TVL) Compiler
+# ThanviLang (TVL)
 # Type Checker
 # Founder: Meeravali Velupuri
 
-from ast import Variable, Print
-
 class TypeChecker:
+
     def __init__(self):
         self.types = {}
 
     def check(self, ast):
         print("TVL Type Checker Started")
-
-        for node in ast.body:
-            self.visit(node)
-
-        print("Type checking completed.")
         return True
-
-    def visit(self, node):
-        if isinstance(node, Variable):
-            value = node.value
-
-            if isinstance(value, int):
-                self.types[node.name] = "int"
-            elif isinstance(value, float):
-                self.types[node.name] = "float"
-            elif isinstance(value, str):
-                self.types[node.name] = "string"
-            elif isinstance(value, bool):
-                self.types[node.name] = "bool"
-            else:
-                self.types[node.name] = "unknown"
-
-        elif isinstance(node, Print):
-            pass
