02aeae4afe678a142fcf5c52b3bff07754676347
Vhydjnb <113295706+Vhydjnb@users.noreply.github.com>
2026-07-12 17:09:35 +0530

    Implement basic TVL Parser

--- compiler/parser.py
+++ compiler/parser.py
@@ -1,17 +1,22 @@
-# ThanviLang (TVL) Compiler
+# ThanviLang (TVL)
 # Parser
 # Founder: Meeravali Velupuri
 
 from lexer import Lexer
 
+
 class Parser:
+
     def __init__(self, source):
-        self.lexer = Lexer(source)
-        self.tokens = self.lexer.tokenize()
+        self.tokens = Lexer(source).tokenize()
+        self.position = 0
+
+    def current(self):
+        return self.tokens[self.position]
+
+    def advance(self):
+        self.position += 1
 
     def parse(self):
         print("TVL Parser Started")
-        return {
-            "type": "Program",
-            "body": []
-        }
+        return self.tokens
