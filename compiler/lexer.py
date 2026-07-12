ead978f2aea26dcdbdccda0e14a78c1f131067cd
Vhydjnb <113295706+Vhydjnb@users.noreply.github.com>
2026-07-12 17:08:06 +0530

    Improve TVL Lexer tokenization

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -1,6 +1,9 @@
-# ThanviLang (TVL) Compiler
+# ThanviLang (TVL)
 # Lexer
 # Founder: Meeravali Velupuri
+
+from tokens import Token, TokenType
+
 
 class Lexer:
     def __init__(self, source):
@@ -8,5 +11,19 @@ class Lexer:
         self.position = 0
 
     def tokenize(self):
-        print("TVL Lexer Started")
-        return []
+        tokens = []
+
+        words = self.source.split()
+
+        for word in words:
+            if word == "store":
+                tokens.append(Token(TokenType.STORE, word, 1))
+            elif word == "show":
+                tokens.append(Token(TokenType.SHOW, word, 1))
+            elif word.isdigit():
+                tokens.append(Token(TokenType.NUMBER, int(word), 1))
+            else:
+                tokens.append(Token(TokenType.IDENTIFIER, word, 1))
+
+        tokens.append(Token(TokenType.EOF, "", 1))
+        return tokens
